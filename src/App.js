fd755c8a904ef80968e7246bed9a1c708c20d55d
Novell Morris <lnovell30@gmail.com>
2020-07-09 22:55:32 -0700

    added routing to the system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+
+const HatsPage = () => (
+    <div>
+      <h1> HATS PAGE </h1>
+    </div>
+)
 function App() {
   return (
     <div>
-     <HomePage />
+      <switch>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/hats' component={HatsPage} />
+      </switch>
     </div>
   );
 }
